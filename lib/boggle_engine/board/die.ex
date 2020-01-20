afb1d3888fc2d729dd9229e548246b558c59162b
rtvu <rtvu87@gmail.com>
2020-01-20 09:06:06 +0000

    Implemented die representation

---
+++ lib/boggle_engine/board/die.ex
@@ -0,0 +1,36 @@
+defmodule BoggleEngine.Board.Die do
+  @moduledoc false
+
+  # Die is specified by a list of graphemes. Each capital letter signals a
+  # different face of the die. Die is represented by a 6 element string list.
+
+  # Converts a specification to a die.
+  def make_die(specification) do
+    make_die(specification, [])
+  end
+
+  defp make_die(specification, result)
+
+  defp make_die([], result) do
+    result
+  end
+
+  defp make_die([rest], result) do
+    [rest | result]
+  end
+
+  defp make_die([first, second | rest], result) do
+    # A die face will either have a single capital grapheme or a capital
+    # grapheme followed by a lowercase grapheme.
+    if uppercase?(second) do
+      make_die([second | rest], [first | result])
+    else
+      make_die(rest, [first <> second | result])
+    end
+  end
+
+  # Determines if letter is uppercase.
+  defp uppercase?(letter) do
+    letter == String.upcase(letter)
+  end
+end
---
+++ test/boggle_engine/board/die_test.exs
@@ -0,0 +1,24 @@
+defmodule BoggleEngine.Board.DieTest do
+  use ExUnit.Case
+
+  alias BoggleEngine.Board.Die
+
+  defp format_die(specification) do
+    specification
+    |> String.graphemes()
+    |> Die.make_die()
+    |> Enum.sort()
+  end
+
+  test "simple die" do
+    assert format_die("AAEEGN") == Enum.sort(["A", "A", "E", "E", "G", "N"])
+    assert format_die("ABBJOO") == Enum.sort(["A", "B", "B", "J", "O", "O"])
+    assert format_die("ACHOPS") == Enum.sort(["A", "C", "H", "O", "P", "S"])
+  end
+
+  test "complex die" do
+    assert format_die("HIMNUQu") == Enum.sort(["H", "I", "M", "N", "U", "Qu"])
+    assert format_die("AnErHeInQuTh") == Enum.sort(["An", "Er", "He", "In", "Qu", "Th"])
+    assert format_die("JKQuWXZ") == Enum.sort(["J", "K", "Qu", "W", "X", "Z"])
+  end
+end
