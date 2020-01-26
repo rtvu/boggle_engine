9d6c456a732276ca2222653581c089dad0d70dd4
rtvu <rtvu87@gmail.com>
2020-01-26 07:49:59 +0000

    Changed solve function's parameters

--- test/boggle_engine/board/solver_test.exs
+++ test/boggle_engine/board/solver_test.exs
@@ -21,4 +21,24 @@ defmodule BoggleEngine.Board.SolverTest do
 
     assert MapSet.new(solved_words) == MapSet.new(["ate", "eat", "eta", "tae", "tazze", "tea", "zeta"])
   end
+
+  test "custom lexicon succeed" do
+    lexicon = Lexicon.new(["ail", "sail"])
+    board = Board.from_string("SAILZZZZZZZZZZZZ", :boggle)
+    rules = :standard
+    minimum_word_length = 3
+    solved_words = Solver.solve(board, rules, minimum_word_length, lexicon: lexicon)
+
+    assert MapSet.new(solved_words) == MapSet.new(["ail", "sail"])
+  end
+
+  test "custom lexicon fail" do
+    lexicon = Lexicon.new(["ail"])
+    board = Board.from_string("SAILZZZZZZZZZZZZ", :boggle)
+    rules = :standard
+    minimum_word_length = 3
+    solved_words = Solver.solve(board, rules, minimum_word_length, lexicon: lexicon)
+
+    refute MapSet.new(solved_words) == MapSet.new(["ail", "sail"])
+  end
 end
